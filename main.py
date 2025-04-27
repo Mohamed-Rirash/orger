cbeff41e1bf653e2b0e20e57b4ea1518b2953a8e
Mohamed Rirash <codesavvylabs@gmail.com>
2025-04-27 11:21:47 +0300

    source directory handl is added

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
+from pathlib import Path
+
 import typer
 
 app = typer.Typer()
 
+DEFAULT_DOWNLOADS = Path.home() / "Downloads"
 
-@app.command(help="starting")
-def hello():
-    typer.echo("hello")
+
+@app.command()
+def auto(
+    src: Path = typer.Option(
+        DEFAULT_DOWNLOADS,
+        "--src",
+        "-s",
+        help="Source folder (defaults to your Downloads).  You can also do `--src ~/Desktop` or give an absolute path.",
+        exists=True,
+        file_okay=False,
+        dir_okay=True,
+        readable=True,
+        resolve_path=True,  # expanduser + realpath
+    ),
+):
+    typer.echo(f"Organizing files in {Path.home() / src}")
 
 
 if __name__ == "__main__":
-    typer.run(hello)
+    app()
